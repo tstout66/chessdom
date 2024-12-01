689e5566d891508d96ae7488d14aa9329c863814
Taylor Stout <taylor.stout@searchdiscovery.com>
2024-12-01 14:20:13 -0500

    revise

--- board.gd
+++ board.gd
@@ -1,9 +1,25 @@
-extends Node
+extends Node2D
 class_name Board
 
 var active_tiles: Array[Tile]
 var tile_size: int = 25
 enum TileColor {WHITE, BLACK}
+var board_size: int = 15
+
+func _init() -> void:
+	_draw()
+
+func _draw():
+	var row_index := board_size
+	for i in board_size:
+		draw_row(i)
+
+func draw_row(row: int):
+	for i in board_size:
+		if ((i % 2) - (row % 2)) == 0:
+			draw_rect(Rect2(i*tile_size, row * tile_size, tile_size, tile_size), Color.WEB_GRAY)
+		else:
+			draw_rect(Rect2(i*tile_size, row * tile_size, tile_size, tile_size), Color.BLACK)
 
 class Tile:
 	var tile_loc: Vector2
--- main.gd
+++ main.gd
@@ -2,23 +2,23 @@ extends Node2D
 
 var board_size: int = 32
 var tile_size: int = 50
-static var board: Board
 
+#func _enter_tree() -> void:
+	#var board = board_scene.instantiate()
+	#add_child(board)
 
+#func _ready() -> void:
+	#var board = board_scene.instantiate()
+	#add_child(board)
 
-func _draw():
-	var row_index := board_size
-	for i in board_size:
-		draw_row(i)
-	
-	#draw_rect(Rect2(0.0, 0.0, 50.0, 50.0), Color.WEB_GRAY)
-	#draw_rect(Rect2(50.0, 50.0, 50.0, 50.0), Color.WEB_GRAY)
-	#draw_rect(Rect2(0.0, 50.0, 50.0, 50.0), Color.BLACK)
-	#draw_rect(Rect2(50.0, 0.0, 50.0, 50.0), Color.BLACK)
-
-func draw_row(row: int):
-	for i in board_size:
-		if ((i % 2) - (row % 2)) == 0:
-			draw_rect(Rect2(i*tile_size, row * tile_size, tile_size, tile_size), Color.WEB_GRAY)
-		else:
-			draw_rect(Rect2(i*tile_size, row * tile_size, tile_size, tile_size), Color.BLACK)
+#func _draw():
+	#var row_index := board_size
+	#for i in board_size:
+		#draw_row(i)
+#
+#func draw_row(row: int):
+	#for i in board_size:
+		#if ((i % 2) - (row % 2)) == 0:
+			#draw_rect(Rect2(i*tile_size, row * tile_size, tile_size, tile_size), Color.WEB_GRAY)
+		#else:
+			#draw_rect(Rect2(i*tile_size, row * tile_size, tile_size, tile_size), Color.BLACK)
