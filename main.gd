00e7223063af519bf4997a742488bd8608db91ce
Taylor Stout <taylor.stout@hcl.com>
2024-12-01 13:20:40 -0500

    board build cont.

--- main.gd
+++ main.gd
@@ -1,7 +1,24 @@
 extends Node2D
 
+var board_size: int = 32
+var tile_size: int = 50
+static var board: Board
+
+
+
 func _draw():
-	draw_rect(Rect2(0.0, 0.0, 25.0, 25.0), Color.GREEN, true)
-	draw_rect(Rect2(25.0, 25.0, 25.0, 25.0), Color.GREEN, true)
-	draw_rect(Rect2(0.0, 25.0, 25.0, 25.0), Color.GREEN, false)
-	draw_rect(Rect2(25.0, 0.0, 25.0, 25.0), Color.GREEN, false)
+	var row_index := board_size
+	for i in board_size:
+		draw_row(i)
+	
+	#draw_rect(Rect2(0.0, 0.0, 50.0, 50.0), Color.WEB_GRAY)
+	#draw_rect(Rect2(50.0, 50.0, 50.0, 50.0), Color.WEB_GRAY)
+	#draw_rect(Rect2(0.0, 50.0, 50.0, 50.0), Color.BLACK)
+	#draw_rect(Rect2(50.0, 0.0, 50.0, 50.0), Color.BLACK)
+
+func draw_row(row: int):
+	for i in board_size:
+		if ((i % 2) - (row % 2)) == 0:
+			draw_rect(Rect2(i*tile_size, row * tile_size, tile_size, tile_size), Color.WEB_GRAY)
+		else:
+			draw_rect(Rect2(i*tile_size, row * tile_size, tile_size, tile_size), Color.BLACK)
